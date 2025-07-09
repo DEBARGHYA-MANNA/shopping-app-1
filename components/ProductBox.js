0f1d1df9f6932e4978b5bf9840af8375ea7784ec
DEBARGHYA-MANNA <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>
2025-07-09 17:00:57 +0530

    productcard UI changed

--- components/ProductBox.js
+++ components/ProductBox.js
@@ -2,14 +2,25 @@
 
 export default function ProductCard({ product, onAdd }) {
     return (
-        <div className="bg-[#14B8A6] shadow-md rounded-lg p-4 flex flex-col justify-between h-full">
+        <div className="bg-[#14B8A6] shadow-md rounded-lg p-4 flex flex-col justify-between h-full text-[#1E293B]">
             <img
                 src={product.image}
                 alt={product.title}
                 className="h-40 object-contain mx-auto mb-4"
             />
-            <h2 className=" text-[#1E293B] text-md font-semibold mb-2 line-clamp-2">{product.title}</h2>
-            <p className="text-[#1E293B] font-bold mb-4">₹{product.price.toFixed(2)}</p>
+
+            <h2 className="text-md text-[#1E293B] font-semibold mb-2 line-clamp-2">
+                {product.title}
+            </h2>
+
+            <p className="text-sm text-[#1E293B] mb-2">{product.description}</p>
+
+            <div className="text-sm font-bold text-[#1E293B] mb-3 flex justify-between items-center">
+                ⭐ {product.rating?.rate} / 5 &nbsp;·&nbsp; {product.rating?.count} reviews
+                <p className="font-bold text-[#1E293B] mb-3">₹{product.price.toFixed(2)}</p>
+            </div>
+
+            
 
             <button
                 onClick={onAdd}
